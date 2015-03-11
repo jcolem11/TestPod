341ab8e6180f5a3fc9aa08649f401d285454bb0c
Joshua Coleman <joshc@friendsoftheweb.com>
2015-03-11 13:36:06 -0400

    4uth4ut
    Signed-off-by: Joshua Coleman <joshc@friendsoftheweb.com>

---
+++ Example/Pods/Target Support Files/Pods-Otherpod-Otherpod/Pods-Otherpod-Otherpod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Otherpod_Otherpod : NSObject
+@end
+@implementation PodsDummy_Pods_Otherpod_Otherpod
+@end
---
+++ Example/Pods/Target Support Files/Pods-Otherpod/Pods-Otherpod-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Otherpod
+#define COCOAPODS_POD_AVAILABLE_Otherpod
+#define COCOAPODS_VERSION_MAJOR_Otherpod 0
+#define COCOAPODS_VERSION_MINOR_Otherpod 1
+#define COCOAPODS_VERSION_PATCH_Otherpod 0
+
