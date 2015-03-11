efa73f5c732470294deda7404def98eb22ef1b37
Joshua Coleman <joshc@friendsoftheweb.com>
2015-03-11 14:15:09 -0400

    changed podspec
    Signed-off-by: Joshua Coleman <joshc@friendsoftheweb.com>

---
+++ Pod/Classes/class.h
@@ -0,0 +1,13 @@
+//
+//  class.h
+//  Pods
+//
+//  Created by Joshua Coleman on 3/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface class : UIView
+
+@end
---
+++ Pod/Classes/class.m
@@ -0,0 +1,21 @@
+//
+//  class.m
+//  Pods
+//
+//  Created by Joshua Coleman on 3/11/15.
+//
+//
+
+#import "class.h"
+
+@implementation class
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
